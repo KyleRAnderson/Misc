330daf927ab838fd8353f06fff00f55bf9443828
eAUE <kra.aue@gmail.com>
2016-10-12 21:39:49 -0400

    Improved the system
    Now the user only has a total of 3 attempts at guessing the correct
    number.

--- Numberguesser.py
+++ Numberguesser.py
@@ -1,14 +1,14 @@
 # Goal: Make a program that generates a number and then has user guess it, with only three attempts.
 from random import randint
 rndm= randint(0,100)
-gNum = 4
+gNum = 3
 while gNum > 0:
     guess= (input('Insert your guess number here:')) #User puts in their number
     if str.isnumeric(guess) == True:
         int(guess)
     else:
         gNum -= 1
-        print('I\'m sorry, that\' not a valid number... Try again. You have', (gNum), 'attempt(s) left.')
+        print('I\'m sorry, that\'s not a valid number... Try again. You have', (gNum), 'attempt(s) left.')
         continue
     if guess == rndm:
         print('Success! You have guessed the number', rndm)
